ff487404790b6781bacdd4d3a52cec135425519e
Jose Ruivo <jruivo.dev@gmail.com>
2017-04-02 23:52:05 +0100

    Added blank activity. Added progress bar.

--- app/src/main/java/com/jruivodev/mypokedex/PokemonActivity.java
+++ app/src/main/java/com/jruivodev/mypokedex/PokemonActivity.java
@@ -4,7 +4,9 @@ import android.app.LoaderManager;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,8 @@ public class PokemonActivity extends AppCompatActivity implements LoaderManager.
     private PokemonAdapter mAdapter;
     private final static int POKEMON_LOADER = 1;
     private final static String POKEMON_URL = "http://pokeapi.co/api/v2/pokemon/";
+    private TextView mEmptyStateTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,11 @@ public class PokemonActivity extends AppCompatActivity implements LoaderManager.
         mAdapter = new PokemonAdapter(this, new ArrayList<Pokemon>());
         listView.setAdapter(mAdapter);
 
+
+        //Setting the empty view
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        listView.setEmptyView(mEmptyStateTextView);
+
     }
 
     @Override
@@ -38,6 +47,11 @@ public class PokemonActivity extends AppCompatActivity implements LoaderManager.
 
     @Override
     public void onLoadFinished(Loader<List<Pokemon>> loader, List<Pokemon> data) {
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        mEmptyStateTextView.setText("No pokemons found.");
+
         mAdapter.clear();
         if (data != null && !data.isEmpty())
             mAdapter.addAll(data);
